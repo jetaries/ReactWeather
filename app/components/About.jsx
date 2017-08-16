f40eb625b3e57ce201266ac55dee0b0e39534302
Gordon Chen <jetaries@gmail.com>
2017-08-16 19:17:47 +0800

    style about page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,16 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h3>About Component</h3>
+			<h1 className="text-centered">About</h1>
+			<p>Here are the resources we used:</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org">Open Weather Map</a> - I used open weather map to search for weather data by city name.
+				</li>
+			</ul>
 		</div>
 	);
 };
